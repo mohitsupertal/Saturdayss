f179e53b43b209f5b17d56fd1692e4e515aded77
PoojaSri <poojasrivastava@supertal.io>
2022-04-18 14:41:21 +0530

    crash on login resolved

--- app/src/main/java/com/shock/saturdaylifestyle/UnravelApp.kt
+++ app/src/main/java/com/shock/saturdaylifestyle/UnravelApp.kt
@@ -1,10 +1,16 @@
 package com.shock.saturdaylifestyle
 
+import android.content.Context
+import com.google.firebase.FirebaseApp
 import com.shock.saturdaylifestyle.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class UnravelApp : DaggerApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent
             .builder()
